d03ced841b534c592f88d8f4482ca880ae31a13a
Sarawut Phaekuntod <sarawut@idevs.work>
2026-05-23 16:36:49 +0700

    fix: null-guard utils/date updateDateProxyValue and harden email regex range

--- src/helpers/dialogHelper.ts
+++ src/helpers/dialogHelper.ts
@@ -14,7 +14,7 @@ export function removeSelect2ClearButton(...targets: string[]): void {
 export function EmailValidator(value: string): string | null {
    
   const filter =
-    /^([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+    /^([\w.-]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
   return filter.test(value) ? null : 'Invalid email address'
 }
 
